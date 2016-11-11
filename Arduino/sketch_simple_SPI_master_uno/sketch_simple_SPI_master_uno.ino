63b69bb55e0cbccfba3e2e4fa1f6994e81b0d7fd
nova <nova@gpm.net>
2016-11-11 18:38:17 +0900

    spi test

--- Arduino/sketch_simple_SPI_master_uno/sketch_simple_SPI_master_uno.ino
+++ Arduino/sketch_simple_SPI_master_uno/sketch_simple_SPI_master_uno.ino
@@ -1,20 +1,27 @@
-
 #include <SPI.h>
+char received = 'K';
 
-#define SS 10
-
-void setup() 
+void setup (void)
 {
-   Serial.begin(115200);
-   SPI.begin();
-   pinMode(SS,OUTPUT);
-   digitalWrite(SS,HIGH);
+  SPI.begin (); // SPI 통신 초기화
+  digitalWrite(SS, HIGH);   // SS = 10 : 슬레이브가 선택되지 않은 상태로 유지
+  //SPI.setClockDivider(SPI_CLOCK_DIV16);  // 안정적인 전송을 위해 분주비를 높여 전송 속도를 낮춤
+  Serial.begin(115200);
+}
+void loop (void)
+{
+  if(Serial.available()){
+    char data = Serial.read(); // 데이터 입력 확인
+    if(data == 'F')
+    {
+      while(received == 'K')
+      {
+        digitalWrite(SS, LOW); // 슬레이브를 선택한다.
+        char received = SPI.transfer(received + 1); // 1바이트 데이터 수신을 위해 의미 없는 1바이트 데이터를 전송한다.
+        digitalWrite(SS, HIGH); // 슬레이브 선택을 해제한다.
+        Serial.println(received);
+      }
+    }
+  }
 }
 
-void loop()
-{
-   byte recv = SPI.transfer((byte)'E');
-   
-   Serial.print("RECV : "); Serial.println(recv);
-   delay(1000);
-}
--- Arduino/sketch_simple_SPI_slave_due/sketch_simple_SPI_slave_due.ino
+++ Arduino/sketch_simple_SPI_slave_due/sketch_simple_SPI_slave_due.ino
@@ -2,17 +2,31 @@
 #include <SPI.h>
 
 #define SS 53
+int idx = 0;
+byte SendByte[100] = {'t', 'e', 's', 't', '-','s','l','a','v','e','r','!', };
+byte ReadByte[100] = {'0', };
 
 void setup() 
 {
-   SPI.begin();
-   pinMode(SS,INPUT);
+   SPI.begin(SS);
+   //pinMode(SS,INPUT); // ????
    Serial.begin(115200);
 }
 
 void loop()
 {
-   char c = SPI.transfer(SS,0x45,SPI_CONTINUE);
-   Serial.println(c);
-   delay(1000);
+  byte s = SPI.transfer(SS, 0x00);
+  if(s == 'K')
+  { 
+      Serial.println("master data received... ");
+      for(int i = 0; i < 15; i++)
+      {
+        ReadByte[i] = SPI.transfer(SS, SendByte[i], SPI_CONTINUE);
+         Serial.print(idx); Serial.print(" : "); Serial.println(ReadByte[i]);
+      }
+       SPI.transfer(SS, 0x00);    // continue mode 종료
+       delay(1000);
+       idx++;
+  }
+   memset(ReadByte, 0, 100);
 }
