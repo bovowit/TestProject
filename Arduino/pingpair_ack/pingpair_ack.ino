741f93a893e129b20465aff35640c23d9bd02e82
nova <nova@gpm.net>
2016-12-09 18:40:52 +0900

    senser ack

--- Arduino/pingpair_ack/pingpair_ack.ino
+++ Arduino/pingpair_ack/pingpair_ack.ino
@@ -1,39 +1,28 @@
-/*
-// March 2014 - TMRh20 - Updated along with High Speed RF24 Library fork
-// Parts derived from examples by J. Coliz <maniacbug@ymail.com>
-*/
-/**
-* Example for efficient call-response using ack-payloads
-*
-* This example continues to make use of all the normal functionality of the radios including
-* the auto-ack and auto-retry features, but allows ack-payloads to be written optionally as well.
-* This allows very fast call-response communication, with the responding radio never having to
-* switch out of Primary Receiver mode to send back a payload, but having the option to if wanting
-* to initiate communication instead of respond to a commmunication.
-*/
 
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 
-// Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 
+// translate
 RF24 radio(7, 8);
 
-// Topology
 const uint64_t pipes[2] = { 0xABCDABCD71LL, 0x544d52687CLL };              // Radio pipe addresses for the 2 nodes to communicate.
-                                                                           // Role management: Set up role.  This sketch uses the same software for all the nodes
-                                                                           // in this system.  Doing so greatly simplifies testing.  
-
 typedef enum { role_ping_out = 1, role_pong_back } role_e;                 // The various roles supported by this sketch
-const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back" }; // The debug-friendly names of those roles
+const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back" };  // The debug-friendly names of those roles
 role_e role = role_pong_back;                                              // The role of the current running sketch
-                                                                           // A single byte to keep track of the data being sent back and forth
-byte counter = 1;
-byte data[32];
-byte rBuff[100];
 
-void setup() 
+                                       // data 
+byte counter = 1;
+byte data[32] = { 0, };
+byte rBuff[32] = { 0, };
+int sensitivity = 10;
+unsigned long synctime = 300000; // 30 msec
+unsigned long stime = 0;
+unsigned long currenttime = 0;
+bool bOneShot = false;
+
+void setup()
 {
   Serial.begin(115200);
   printf_begin();
@@ -52,55 +41,73 @@ void setup()
   radio.startListening();                 // Start listening
   radio.printDetails();                   // Dump the configuration of the rf unit for debugging
 
-  for (int i = 0; i<32; i++) 
-    data[i] = random(255);               //Load the buffer with random data
+  stime = micros();
+
 }
 
-void loop(void) 
+void loop(void)
 {
-  if (role == role_ping_out) 
+  if (role == role_ping_out)
   {
-    radio.stopListening();                                  // First, stop listening so we can talk.
-
-    printf("Now sending %d as payload. ", counter);
-    byte gotByte;
-    unsigned long time = micros();                          // Take the time, and send it.  This will block until complete   
-                                                            //Called when STANDBY-I mode is engaged (User is finished sending)
-    //if (!radio.write(&counter, 1)) 
-    if (!radio.writeFast(&data, 32)) 
-      Serial.println(F("failed."));
-    else 
+    if (radio.available())
     {
-      if (!radio.available()) 
-        Serial.println(F("Blank Payload Received."));
-      else 
+      //while (radio.available())
+      //{
+      radio.read(&rBuff, 32);
+      printf("RECV command : %s from master \n\r", (char*)&rBuff);
+      if (rBuff[0] == 0x22)
       {
-        while (radio.available()) 
-        {
-          unsigned long tim = micros();
-          radio.read(&gotByte, 1);
-          printf("Got response %d, round-trip delay: %lu microseconds\n\r", gotByte, tim - time);
-          counter++;
-        }
+        currenttime = micros();
+        radio.writeFast(&currenttime, 32);
+        Serial.println("answer sync time ............");
       }
+      //}
     }
-    delay(1000);
+
+    int _value = analogRead(A0);
+    if (_value > sensitivity && bOneShot == false)
+    {
+      bOneShot = true;
+      stime = micros();
+      printf("Detected Impact - %d : time %d \n\r", _value, stime);
+    }
+    else
+    {
+      if (micros() - stime > synctime)   // 30 msec 마다 무효화, 노이즈 처리
+      {
+        stime = 0;
+        bOneShot = false;
+      }
+      return;
+    }
+
+    //memcpy(data, stime, sizeof(stime));
+    radio.stopListening();                                  // First, stop listening so we can talk.
+    if (!radio.writeFast(&stime, 32))// &data, 32))
+    {
+      Serial.println(F("failed. clear data "));
+      memset(&data, 0, sizeof(stime));
+    }
+
   }
 
+
   // Pong back role.  Receive each packet, dump it out, and send it back
-  if (role == role_pong_back) 
+  if (role == role_pong_back)
   {
     byte pipeNo;
     byte gotByte;                                       // Dump the payloads until we've gotten everything
-    while (radio.available(&pipeNo)) 
+    while (radio.available(&pipeNo))
     {
-      //radio.read(&gotByte, 1);
-      //radio.writeAckPayload(pipeNo, &gotByte, 1);
       radio.read(&rBuff, 32);
-      radio.writeAckPayload(pipeNo, &rBuff, 1);
+      //radio.writeAckPayload(pipeNo, &rBuff, 1);
     }
-    String sData(rBuff[0]);
-    Serial.print("[RECV]"); Serial.println(sData);
+    unsigned long impact_time = 0;
+    memcpy(&impact_time, rBuff, sizeof(unsigned long));
+    if(impact_time > 0)
+    {
+      Serial.print("[RECV] "); Serial.print(impact_time); Serial.print(" : "); Serial.println((char*)&rBuff);
+    }
   }
 
   // Change roles
@@ -118,6 +125,7 @@ void loop(void)
     else if (c == 'R' && role == role_ping_out)
     {
       Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));
+
       role = role_pong_back;                // Become the primary receiver (pong back)
       radio.openWritingPipe(pipes[1]);
       radio.openReadingPipe(1, pipes[0]);
