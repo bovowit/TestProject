dcea7c3e97a851e4b8979af23b1a77e1e6ab0bb0
nova <nova@gpm.net>
2017-01-04 15:45:33 +0900

    asdf

--- Arduino/sketch_ADC_fast_oslio/sketch_ADC_fast_oslio.ino
+++ Arduino/sketch_ADC_fast_oslio/sketch_ADC_fast_oslio.ino
@@ -1,71 +1,159 @@
-int gSensitivity = 800;
-int a0 = 0, a1 = 0, a2 = 0, a3 = 0;
-int aVal[4][100];
-unsigned long firsttime = 0;
+#include <SPI.h>
+#include <SimpleTimer.h>
+#include <Limits.h>
+#include "printf.h"
 
-void setup() 
+typedef enum { role_sensor = 1, role_controller } role_e;
+
+// 필수 데이터 셋팅
+role_e g_role = role_controller;
+const int myindex = 0; // 0~ 센서의 갯수만큼 위치에 따라 차례로 설정해야 함.
+const int sensorcnt = 2;
+
+// 디지털핀 시그널을 통해 시간 동기화
+int gTimeSyncPin = 9;   // 두에의 경우 모든 핀에 인터럽트 가능.
+int gTimeSyncRecvPin[4] = {2, 3, 21, 20};  // 우노의 경우 2, 3번만 인터럽트 가능 => 메가로 변경 필요(2, 3, 21, 20, 19, 18)
+
+SimpleTimer sync_timer;
+int _timer_id = 0;
+
+int sensitivity = 650;
+unsigned long synctime = 1000000; // 300 msec
+unsigned long stime = 0;
+unsigned long startreadingtime = 0;
+bool bOneShot = false;
+volatile int iReadSensorCnt = 0;
+
+void BaseSetting(role_e role)
 {
-  //empty();
-  Serial.begin(9600);
-  int t = analogRead(0); t = analogRead(1); t = analogRead(2); t = analogRead(3);
-
-  ADC->ADC_MR |= 0x80; // these lines set free running mode on adc 7 and adc 6 (pin A0 and A1 - see Due Pinout Diagram thread)
-  ADC->ADC_CR = 2;
-  //ADC->ADC_CHER = 0xC0; // this is (1<<7) | (1<<6) for adc 7 and adc 6     
-  ADC->ADC_CHER=0xF0; // this is (1<<7) | (1<<6) for adc 7 and adc 4                                     
-}
-
-void loop() 
-{
-  if (Serial.available() > 0)
+  if (role == role_sensor)
   {
-    char temp_buff[30] = { 0, };
-    int length = Serial.readBytes(temp_buff, 30);
-    String sReadData = temp_buff;
-    gSensitivity = sReadData.toInt();
-    Serial.print("Sensitivity value input : "); Serial.println(sReadData);
+    Serial.println("Base Pipe Setting : role_sensor");
+    pinMode(gTimeSyncPin, INPUT_PULLUP);  
+    attachInterrupt(digitalPinToInterrupt(gTimeSyncPin), SensorInterrupt, RISING);
+    pinMode(gTimeSyncRecvPin[myindex], OUTPUT);    
   }
-
-    for (int i = 0; i<99; i++) {
-      while ((ADC->ADC_ISR & 0x80) == 0);
-      while ((ADC->ADC_ISR & 0x80) == 0);// wait for 2 conversions
-      while ((ADC->ADC_ISR & 0x80) == 0);// wait for 3 conversions
-      while ((ADC->ADC_ISR & 0x80) == 0);// wait for 4 conversions
-      aVal[0][i] = ADC->ADC_CDR[7];              // read data on A0 pin
-      aVal[1][i] = ADC->ADC_CDR[6];              // read data on A1 pin
-      if((aVal[0][0] > gSensitivity || aVal[1][0] > gSensitivity) && firsttime == 0)
-          firsttime = micros();
-      else
-          break;
-      aVal[2][i] = ADC->ADC_CDR[5];              // read data on A2 pin
-      aVal[3][i] = ADC->ADC_CDR[4];              // read data on A3 pin
-    }
-  unsigned long tbase = micros();
-  if(aVal[0][0] > gSensitivity || aVal[1][0] > gSensitivity)
+  else if (role == role_controller)
   {
-        Serial.println("--------------- micros");
-       Serial.print("A0 : "); 
-     for(int i = 0; i < 100; i++)
-    {
-        Serial.print(aVal[0][i]); Serial.print("|");
-     }
-      Serial.println("+");
-    Serial.print("A1 : ");
-    for(int i = 0; i < 100; i++)
-    {
-       Serial.print(aVal[1][i]);  Serial.print("|");
-    }
-    delay(10);
-    Serial.println("+");
+    Serial.println("Base Pipe Setting : role_controller");
+    pinMode(gTimeSyncPin, OUTPUT);
+  for (int i = 0; i < sensorcnt; i++)
+    pinMode(gTimeSyncRecvPin[i], INPUT_PULLUP); // 인터럽트 핀으로 사용
+  attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[0]), TimeInterruptRecv0, RISING);
+  attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[1]), TimeInterruptRecv0, RISING);
+  attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[2]), TimeInterruptRecv0, RISING);
+  attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[3]), TimeInterruptRecv0, RISING);
 
-   }
-
-  if(abs(micros() - firsttime) > 300)
-  {
-       for(int i = 0; i < 100; i++)
-     {
-      aVal[0][i] = 0;
-      aVal[1][i] = 0;
-     }  
+   _timer_id = sync_timer.setInterval(60000, KeepAliveCallback);  // 1분 타이머
+    Serial.print(_timer_id); Serial.println(" : keep alive Timer Started");
   }
 }
+
+void SensorInterrupt()  // 센스쪽에서 발생. 리셋등에 활용
+{
+}
+
+volatile unsigned long recvtime[4] = {0, };
+void TimeInterruptRecv0()
+{
+  recvtime[0] = micros();
+  iReadSensorCnt++;
+  //Serial.print(recvtime[0]);  Serial.println("  interrupt pin2");
+}
+void TimeInterruptRecv1()
+{
+  recvtime[1] = micros();
+  iReadSensorCnt++;
+  //Serial.print(recvtime[1]); Serial.println("  interrupt pin3");
+}
+void TimeInterruptRecv2()
+{
+  recvtime[2] = micros();
+  iReadSensorCnt++;
+}
+void TimeInterruptRecv3()
+{
+  recvtime[3] = micros();
+  iReadSensorCnt++;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  printf_begin();
+  if (g_role == role_controller)
+    Serial.println(F("*** Selected MainController mode ***"));
+  else
+  {
+    Serial.print(F("*** Selected Sensor mode ***  Sensor Index : "));
+    Serial.print(myindex); Serial.println("");
+  }
+
+  // Setup and configure rf radio
+  BaseSetting(g_role);
+  stime = micros();
+}
+
+// Keep alive check
+void KeepAliveCallback()
+{
+}
+
+void loop(void)
+{
+  if (g_role == role_sensor)
+  {
+    int _valuex = analogRead(A0);
+    int _valuey = analogRead(A1);
+    int _valuez = analogRead(A2);     // 효율성을 위해 확인 후 제거.. Z축은 무의미한 값일듯.
+    if ((_valuex > sensitivity || _valuey > sensitivity || _valuez > sensitivity) && bOneShot == false)
+    {
+       bOneShot = true;
+       stime = micros();
+       digitalWrite(gTimeSyncRecvPin[myindex], HIGH);
+       digitalWrite(gTimeSyncRecvPin[myindex], LOW);
+       Serial.print("imapct sensor : "); Serial.println(gTimeSyncRecvPin[myindex]);
+    }
+    if (bOneShot)
+    {
+      if (micros() - stime > synctime)   // 300 msec 마다 무효화, 노이즈 처리
+      {
+        stime = 0;
+        bOneShot = false;
+      }
+    }
+  }
+
+
+  // Pong back role.  Receive each packet, dump it out, and send it back
+  // 메인컨트롤러는 다수의 수집장치로부터 데이터를 수신해서 조합 후 PC로 전송.(최소 4개의 장치중 3개 이상 수집)
+  // 첫번째 신호가 수집된후 30ms 이상이 지나면 데이터 무효화, 새로운 타격으로 처리.
+  if (g_role == role_controller)
+  {
+
+    if (iReadSensorCnt > 0)
+    {
+      if (startreadingtime == 0)
+        startreadingtime = micros();
+
+      if (iReadSensorCnt == sensorcnt)
+      {
+        long _duetime = recvtime[0] - recvtime[1];
+        Serial.print("************* ImpactTime : ");
+        Serial.println(_duetime);
+        iReadSensorCnt = 0;
+        startreadingtime = 0;
+        recvtime[0] = recvtime[1] = recvtime[2] = recvtime[3] = 0;
+      }
+      if (startreadingtime != 0 && micros() - startreadingtime > 30000) // 하나의 값만 수신
+      {
+        iReadSensorCnt = 0;
+        startreadingtime = 0;
+        recvtime[0] = recvtime[1] = recvtime[2] = recvtime[3] = 0;
+        Serial.print("---------------- clear ImpactTime  ");
+      }
+      }
+ 
+  }
+}
+
--- Arduino/vs_impact02/vs_impact02.ino
+++ Arduino/vs_impact02/vs_impact02.ino
@@ -1,9 +1,6 @@
-#include <RF24_config.h>
 #include <SPI.h>
 #include <SimpleTimer.h>
 #include <Limits.h>
-#include "nRF24L01.h"
-#include "RF24.h"
 #include "printf.h"
 
 typedef enum { role_sensor = 1, role_controller } role_e;
@@ -13,11 +10,6 @@ role_e g_role = role_controller;
 const int myindex = 0; // 0~ 센서의 갯수만큼 위치에 따라 차례로 설정해야 함.
 const int sensorcnt = 2;
 
-// translate
-RF24 radio(7, 8);
-const uint64_t pipes[4][2] = { { 0xFF0A10, 0xFF0A11 },{ 0xFF0A20, 0xFF0A21 },{ 0xFF0A30, 0xFF0A31 },{ 0xFF0A40, 0xFF0A41 } };
-const char sCmdTimeSync[] = "_RS_";          // 리셋명령.
-
 // 디지털핀 시그널을 통해 시간 동기화
 int gTimeSyncPin = 9;   // 두에의 경우 모든 핀에 인터럽트 가능.
 int gTimeSyncRecvPin[4] = {2, 3, 21, 20};  // 우노의 경우 2, 3번만 인터럽트 가능 => 메가로 변경 필요(2, 3, 21, 20, 19, 18)
@@ -25,18 +17,10 @@ int gTimeSyncRecvPin[4] = {2, 3, 21, 20};  // 우노의 경우 2, 3번만 인터
 SimpleTimer sync_timer;
 int _timer_id = 0;
 
-// data
-const int sensorbuffsize = 16;
-const int buffsize = 16;
-byte data[sensorbuffsize] = { 0, };    // 송신용
-byte command[buffsize] = { 0, };  // 일반 수신용 (명령어 등)
-byte recv[sensorcnt][sensorbuffsize];
-
 int sensitivity = 650;
 unsigned long synctime = 1000000; // 300 msec
 unsigned long stime = 0;
 unsigned long startreadingtime = 0;
-unsigned long base_sync_time = 0;
 bool bOneShot = false;
 volatile int iReadSensorCnt = 0;
 
@@ -45,48 +29,42 @@ void BaseSetting(role_e role)
   if (role == role_sensor)
   {
     Serial.println("Base Pipe Setting : role_sensor");
-
-    pinMode(gTimeSyncPin, INPUT_PULLUP);  // 인터럽트 핀.
-    attachInterrupt(digitalPinToInterrupt(gTimeSyncPin), TimeInterrupt, RISING);
-    pinMode(gTimeSyncRecvPin[myindex], OUTPUT);
+    pinMode(gTimeSyncPin, INPUT_PULLUP);  
+    attachInterrupt(digitalPinToInterrupt(gTimeSyncPin), SensorInterrupt, RISING);
+    pinMode(gTimeSyncRecvPin[myindex], OUTPUT);		
   }
   else if (role == role_controller)
   {
     Serial.println("Base Pipe Setting : role_controller");
-
     pinMode(gTimeSyncPin, OUTPUT);
-    pinMode(gTimeSyncRecvPin[0], INPUT_PULLUP); // 인터럽트 핀으로 사용
-    pinMode(gTimeSyncRecvPin[1], INPUT_PULLUP);
-    pinMode(gTimeSyncRecvPin[2], INPUT_PULLUP);
-    pinMode(gTimeSyncRecvPin[3], INPUT_PULLUP);
-    // 두에는 핀 번호를 인터럽트로 사용
-    attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[0]), TimeInterruptRecv0, RISING);
-    attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[1]), TimeInterruptRecv1, RISING);
-    attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[2]), TimeInterruptRecv2, RISING);
-    attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[3]), TimeInterruptRecv3, RISING);
+	for (int i = 0; i < sensorcnt; i++)
+		pinMode(gTimeSyncRecvPin[i], INPUT_PULLUP); // 인터럽트 핀으로 사용
+	attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[0]), TimeInterruptRecv0, RISING);
+	attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[1]), TimeInterruptRecv0, RISING);
+	attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[2]), TimeInterruptRecv0, RISING);
+	attachInterrupt(digitalPinToInterrupt(gTimeSyncRecvPin[3]), TimeInterruptRecv0, RISING);
 
-   _timer_id = sync_timer.setInterval(3000, SyncTimerCallback);
-    Serial.print(_timer_id); Serial.println(" : Timer Started");
-
+   _timer_id = sync_timer.setInterval(60000, KeepAliveCallback);	// 1분 타이머
+    Serial.print(_timer_id); Serial.println(" : keep alive Timer Started");
   }
 }
 
-void TimeInterrupt()  // 센스쪽에서 발생. 
+void SensorInterrupt()  // 센스쪽에서 발생. 리셋등에 활용
 {
 }
 
 volatile unsigned long recvtime[4] = {0, };
 void TimeInterruptRecv0()
 {
-  iReadSensorCnt++;
   recvtime[0] = micros();
-  Serial.print(recvtime[0]);  Serial.println("  interrupt pin2");
+  iReadSensorCnt++;
+  //Serial.print(recvtime[0]);  Serial.println("  interrupt pin2");
 }
 void TimeInterruptRecv1()
 {
-  iReadSensorCnt++;
   recvtime[1] = micros();
-  Serial.print(recvtime[1]); Serial.println("  interrupt pin3");
+  iReadSensorCnt++;
+  //Serial.print(recvtime[1]); Serial.println("  interrupt pin3");
 }
 void TimeInterruptRecv2()
 {
@@ -116,10 +94,8 @@ void setup()
   stime = micros();
 }
 
-// timer 이벤트가 발생하면 디지털 핀 High setting
-int gTimeSyncRecvCnt = 0;
-bool gFirstTimeSync = true;
-void SyncTimerCallback()
+// Keep alive check
+void KeepAliveCallback()
 {
 }
 
@@ -151,18 +127,31 @@ void loop(void)
 
   // Pong back role.  Receive each packet, dump it out, and send it back
   // 메인컨트롤러는 다수의 수집장치로부터 데이터를 수신해서 조합 후 PC로 전송.(최소 4개의 장치중 3개 이상 수집)
-  // 첫번째 신호가 수집된후 10ms 이상이 지나면 데이터 무효화, 새로운 타격으로 처리.
+  // 첫번째 신호가 수집된후 30ms 이상이 지나면 데이터 무효화, 새로운 타격으로 처리.
   if (g_role == role_controller)
   {
 
-      if (iReadSensorCnt >= sensorcnt)
-      {
-        long _duetime = recvtime[0] - recvtime[1];
-        Serial.print("************* ImpactTime : ");
-        Serial.println(_duetime);
-        iReadSensorCnt = 0;
-               startreadingtime = 0; 
-        recvtime[0] = recvtime[1] = recvtime[2] = recvtime[3] = 0;
+	  if (iReadSensorCnt > 0)
+	  {
+		  if (startreadingtime == 0)
+			  startreadingtime = micros();
+
+		  if (iReadSensorCnt == sensorcnt)
+		  {
+			  long _duetime = recvtime[0] - recvtime[1];
+			  Serial.print("************* ImpactTime : ");
+			  Serial.println(_duetime);
+			  iReadSensorCnt = 0;
+			  startreadingtime = 0;
+			  recvtime[0] = recvtime[1] = recvtime[2] = recvtime[3] = 0;
+		  }
+		  if (startreadingtime != 0 && micros() - startreadingtime > 30000) // 하나의 값만 수신
+		  {
+			  iReadSensorCnt = 0;
+			  startreadingtime = 0;
+			  recvtime[0] = recvtime[1] = recvtime[2] = recvtime[3] = 0;
+			  Serial.print("---------------- clear ImpactTime  ");
+		  }
       }
  
   }
